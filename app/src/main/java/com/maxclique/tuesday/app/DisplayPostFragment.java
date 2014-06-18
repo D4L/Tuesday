bc4d6c892a8fd13a6f5c887c1452df6743c62b01
Austin Lee <Austin.L.D4L@gmail.com>
2014-06-18 01:28:28 -0400

    Add extra styling and server bump

--- app/src/main/java/com/maxclique/tuesday/app/DisplayPostFragment.java
+++ app/src/main/java/com/maxclique/tuesday/app/DisplayPostFragment.java
@@ -9,19 +9,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by Austin on 6/17/2014.
  */
 public class DisplayPostFragment extends Fragment {
 
+    private TextView titleView;
+    private TextView detailsView;
+    private TextView dateView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View resultantView = inflater.inflate(R.layout.display_post_fragment, container, false);
+        titleView = (TextView) resultantView.findViewById(R.id.title);
+        detailsView = (TextView) resultantView.findViewById(R.id.details);
+        dateView = (TextView) resultantView.findViewById(R.id.date);
+
         refresh();
         return resultantView;
     }
@@ -30,7 +39,7 @@ public class DisplayPostFragment extends Fragment {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        // writeListView(getString(R.string.fetching));
+        titleView.setText(getString(R.string.fetching));
         if (networkInfo != null && networkInfo.isConnected()) {
             String postId = getArguments().getString("id", null);
             if (postId == null) {
@@ -42,19 +51,21 @@ public class DisplayPostFragment extends Fragment {
                         public void run(String resultOfTask) {
                             Log.d("Tuesday", resultOfTask);
                             try {
-                                /*writeListView(JSONParser.convertJSONObjects(
-                                        new JSONParser<JSONArray>(resultOfTask,
-                                                new JSONParser.JSONArrayFactory()).getJSON()
-                                ));*/
+                                JSONObject object = new JSONParser<JSONObject>(resultOfTask,
+                                        new JSONParser.JSONObjectFactory()).getJSON();
+                                titleView.setText(object.getString(getString(R.string.subject)));
+                                dateView.setText(object.getString(getString(R.string.created_at)));
+                                detailsView.setText(object.optString(getString(R.string.details),
+                                        ""));
                             } catch (Exception e) {
-                                //writeListView("Failure!");
+                                titleView.setText("Failure!");
                             }
                         }
                     }
             ).execute();
         } else {
             // display
-            // writeListView(getString(R.string.no_network));
+            titleView.setText(getString(R.string.no_network));
         }
     }
 }
--- app/src/main/java/com/maxclique/tuesday/app/ServerURL.java
+++ app/src/main/java/com/maxclique/tuesday/app/ServerURL.java
@@ -4,10 +4,11 @@ package com.maxclique.tuesday.app;
  * Created by Austin on 6/16/2014.
  */
 public class ServerURL {
-    private static final String BASE_URL = "http://maxclique-monday-v1.meteor.com/server/";
+    private static final String BASE_URL = "http://maxclique-monday-v2.meteor.com/server/";
+    private static final String NO_PIC = "?no_pic=true";
 
     public static String getAllPosts() {
-        return BASE_URL + "posts?no_pic=true";
+        return BASE_URL + "posts" + NO_PIC;
     }
-    public static String getPost(String id) {return BASE_URL + "posts/" + id;}
+    public static String getPost(String id) {return BASE_URL + "posts/" + id + NO_PIC;}
 }
